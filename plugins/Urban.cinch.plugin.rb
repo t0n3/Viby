dde7155c3c70f2bfe0ee9ea667aef696b68c8871
Antoine Leroyer <leroyer.antoine.tone@gmail.com>
2014-03-18 11:45:33 +0100

    Urban Dic plugin

---
+++ plugins/Urban.cinch.plugin.rb
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+require 'open-uri'
+require 'nokogiri'
+require 'cgi'
+
+class Urban
+    include Cinch::Plugin
+    
+    set :help => "!urban word"
+
+    match(/(?:urban) (.*)/, :use_prefix => true)
+    def initialize(*args)
+        super
+        @url = "http://www.urbandictionary.com/define.php?term="
+    end
+
+    def execute(m, word)
+        page = Nokogiri::HTML(open("#{@url}#{CGI.escape(word)}"))
+        begin
+          word = page.css(".word")
+          wordDef = page.css(".meaning")
+          m.reply "#{m.user}, « #{word} » : #{wordDef}"
+        rescue 
+          return m.reply "I don't know this word."
+        end
+    end
+end
